8f35ecc1b66e9758240ebf038d05cd8b6bc6a7e4
JSQ <j996730508@163.com>
2025-02-10 17:49:49 +0800

    style: rename file

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -4,9 +4,9 @@ import path from 'path';
 import type { Metadata } from 'next';
 import ErrorResult from '~/components/layouts/error-result';
 
-async function getMDXContent(name: string): Promise<CompileMDXResult<Metadata> | null> {
+async function getMDXContent(slug: string): Promise<CompileMDXResult<Metadata> | null> {
   try {
-    const filePath = path.join(process.cwd(), '/posts/', `${name}.md`);
+    const filePath = path.join(process.cwd(), '/posts/', `${slug}.md`);
     const contents = await readFile(filePath, { encoding: 'utf8' });
     return await compileMDX({ source: contents, options: { parseFrontmatter: true } });
   } catch {
@@ -15,23 +15,23 @@ async function getMDXContent(name: string): Promise<CompileMDXResult<Metadata> |
 }
 
 type Props = {
-  params: Promise<{ name: string }>;
+  params: Promise<{ slug: string }>;
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { name } = await params;
+  const { slug } = await params;
 
-  const res = await getMDXContent(name);
+  const res = await getMDXContent(slug);
   if (!res) return { title: '' };
   const { frontmatter } = res;
   return { title: frontmatter.title };
 }
 
 export default async function Page({ params }: Props) {
-  const { name } = await params;
+  const { slug } = await params;
 
-  const res = await getMDXContent(name);
+  const res = await getMDXContent(slug);
 
   if (!res) return <ErrorResult status="404" back="prev" />;
 
