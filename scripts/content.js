5f67f4e3be9faf3b1a4bdf4b0aa275cc694eed07
JSQ <j996730508@163.com>
2025-02-12 22:19:22 +0800

    feat: add post, add some util, change page style

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-const { mkdirSync, readdirSync, readFileSync, writeFileSync } = require('fs');
+const { mkdirSync, readdirSync, readFileSync, writeFileSync, statSync } = require('fs');
 const { join, basename, extname } = require('path');
 const readingTime = require('reading-time');
 const dayjs = require('dayjs');
@@ -10,9 +10,63 @@ const dayjs = require('dayjs');
  */
 
 /**
+ * @typedef {Object} Toc - 元数据对象。
+ * @property {string} title - 文章标题。
+ * @property {number} level - 标题深度
+ * @property {string} id - 标题的 id，和 html 的 id 对应
+ * @property {Array<Toc>} children - 嵌套标题数据
+ */
+
+/**
+ *
+ * @param {string} markdown md 元数据
+ * @returns {Toc}
+ */
+function extractToc(markdown) {
+  // 定义匹配Markdown标题的正则表达式
+  const headingRegex = /^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)/gm;
+
+  let match;
+  const toc = [];
+  let lastLevel = 0;
+  let stack = [{ children: toc }]; // 初始化栈，根节点有一个空的children数组
+
+  while ((match = headingRegex.exec(markdown)) !== null) {
+    const level = match[1].length; // 标题级别，由#的数量决定
+    const title = match[2].trim(); // 标题文本
+    // id 就是把标题的空格转换成了 -
+    const id = title.toLowerCase().replace(/\s+/g, '-');
+
+    const item = { title: title, level: level, id: id, children: [] };
+
+    if (level > lastLevel) {
+      // 如果当前级别比上一级别深，则进入更深的一层
+      stack[stack.length - 1].children.push(item);
+      stack.push(item); // 更新栈顶为新添加的项
+    } else if (level < lastLevel) {
+      // 如果当前级别比上一级别浅，则返回到较浅的层次
+      for (let i = 0; i < lastLevel - level; i++) {
+        stack.pop(); // 弹出栈顶元素，回到上一层级
+      }
+      stack[stack.length - 1].children.push(item);
+      stack.push(item); // 更新栈顶为新添加的项
+    } else {
+      // 如果当前级别与上一级别相同，则在同一层级添加
+      stack.pop(); // 移除旧的同级项
+      stack[stack.length - 1].children.push(item);
+      stack.push(item); // 更新栈顶为新添加的项
+    }
+
+    lastLevel = level;
+  }
+
+  return toc;
+}
+
+/**
  * 解析 MDX 文件中的 Frontmatter 部分。
  * @param {string} fileContent
- * @returns {{frontMatter: Metadata, content: string}}
+ * @returns {{frontMatter: Metadata, content: string, toc: Toc}}
  */
 function parseFrontmatter(fileContent) {
   const frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
@@ -32,8 +86,9 @@ function parseFrontmatter(fileContent) {
   }
 
   const content = fileContent.replace(frontmatterRegex, '').trim();
+  const toc = extractToc(content);
 
-  return { frontMatter: metadata, content };
+  return { frontMatter: metadata, content, toc };
 }
 
 /**
@@ -57,15 +112,15 @@ function getMDXFiles(dir) {
       }
     });
   }
-  // return readdirSync(dir).filter((file) => extname(file) === '.mdx' || extname(file) === '.md');
   recurse(dir);
   return files;
+  // return readdirSync(dir).filter((file) => extname(file) === '.mdx' || extname(file) === '.md');
 }
 
 /**
  * 读取并解析一个MDX文件的内容。
  * @param {string} filePath - 文件的完整路径。
- * @returns {{frontMatter: Metadata, content: string}} 包含解析出的元数据和正文内容的对象。
+ * @returns {{frontMatter: Metadata, content: string, toc: Toc}} 包含解析出的元数据和正文内容的对象。
  */
 function readMDXFile(filePath) {
   const rawContent = readFileSync(filePath, 'utf-8');
@@ -75,19 +130,17 @@ function readMDXFile(filePath) {
 /**
  * @param {string} dir
  */
-function getMDXData(dir) {
+function createMDXData(dir) {
   const mdxFiles = getMDXFiles(dir);
-  try {
-    mkdirSync('generated', { recursive: true });
-  } catch (e) {
-    console.log('Cannot create folder ', e);
-  }
+
+  mkdirSync('generated', { recursive: true });
+
   writeFileSync(
-    join(process.cwd(), 'generated/content.json'),
+    join(__dirname, '../generated/content.json'),
     JSON.stringify(
       mdxFiles
         .map((file) => {
-          const { frontMatter, content } = readMDXFile(file);
+          const { frontMatter, content, toc } = readMDXFile(file);
           const slug = basename(file, extname(file));
           return {
             ...frontMatter,
@@ -97,14 +150,38 @@ function getMDXData(dir) {
             readingTime: readingTime(content).text,
             wordCount: content.split(/\s+/gu).length,
             content,
+            toc,
           };
         })
         .sort((a, b) => {
           return new Date(b.date).getTime() - new Date(a.date).getTime();
         }),
     ),
+    'utf-8',
   );
   console.log('successfully generated!');
 }
+/**
+ * @param {string} dir
+ */
+function createPostCategory(dir) {
+  const files = readdirSync(dir);
 
-getMDXData(join(process.cwd(), 'posts'));
+  const directories = files
+    .map((file) => {
+      const fullPath = join(dir, file);
+      const stats = statSync(fullPath);
+      if (stats.isDirectory()) {
+        return file;
+      }
+    })
+    .filter((dir) => Boolean(dir)); // 过滤掉非目录项
+
+  mkdirSync('generated', { recursive: true });
+
+  writeFileSync(join(__dirname, '../generated/category.json'), JSON.stringify(directories));
+}
+
+createMDXData(join(__dirname, '../posts'));
+
+createPostCategory(join(__dirname, '../posts'));
