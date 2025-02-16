b27ca979b7ba8faedc23d25d3658c2c6348ddee3
JSQ <j996730508@163.com>
2025-02-16 18:04:57 +0800

    style: adjust toc style

--- components/layouts/toc-tree.tsx
+++ components/layouts/toc-tree.tsx
@@ -4,9 +4,14 @@ import type { Toc } from '~/scripts/content';
 // 主组件接收TOC数据作为props，并渲染整个目录树
 const TocTree = (props: { toc: Toc[] }) => {
   const { toc } = props;
+  if (toc.length == 0) return null;
+  const level = toc[0].level;
 
   return (
-    <ul className="m-0 list-none p-0 text-sm">
+    <ul
+      className="m-0 list-none border-l border-gray-50 p-0 text-sm"
+      style={{ paddingLeft: `${(level - 1) * 0.5}rem` }}
+    >
       {toc.map(
         (item) =>
           // only render h1, h2, h3
@@ -15,7 +20,6 @@ const TocTree = (props: { toc: Toc[] }) => {
               <a
                 className="block text-gray-700 no-underline hover:text-blue-600"
                 href={`#${item.id}`}
-                style={{ paddingLeft: `${(item.level - 1) * 0.8}rem` }}
               >
                 {item.title}
               </a>
