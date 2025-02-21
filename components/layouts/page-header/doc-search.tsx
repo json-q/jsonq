59ec6ab7895051a64e9491dc6aa458eedc61cb97
json-q <112090173+json-q@users.noreply.github.com>
2025-02-21 22:13:27 +0800

    refactor: use flexsearch install of algolia (#8)
    * feat: layout support mobile
    
    * chore: project build and some style
    
    * chore: transform support url when markdown content has local article link
    
    * fix: unable to parse the style label
    
    * style: adjust typography
    
    * style: adjust toc style
    
    * feat: add docker build (#4)
    
    * chore: async main
    
    * refactor: remove algolia dependencis
    
    * refacotor: use flexsearch
    
    * fix: link error, add scrollbar color

--- components/layouts/page-header/doc-search.tsx
+++ components/layouts/page-header/doc-search.tsx
@@ -1,56 +1,106 @@
 'use client';
 import Link from 'next/link';
-import { useCallback, useRef, useState } from 'react';
-import { createPortal } from 'react-dom';
-import { DocSearchModal, useDocSearchKeyboardEvents } from '@docsearch/react';
-import siteConfig from '~/config/siteConfig';
+import { useState } from 'react';
+import debounce from 'lodash.debounce';
+import { File, Heading, Search } from 'lucide-react';
+import { EnrichedDocumentSearchResultSetUnitResultUnit } from 'flexsearch';
 import { Button } from '~/components/ui/button';
-import '@docsearch/css';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
+import { Input } from '~/components/ui/input';
+import searchDoc, { IndexItem } from './search';
 
-export default function AlgoliaDocSearch() {
-  const [isOpen, setIsOpen] = useState(false);
-  const searchButtonRef = useRef<HTMLButtonElement>(null);
+export default function CommandMenu() {
+  const [searchList, setSearchList] = useState<
+    EnrichedDocumentSearchResultSetUnitResultUnit<IndexItem>[]
+  >([]);
 
-  const onOpen = useCallback(() => {
-    setIsOpen(true);
-  }, [setIsOpen]);
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const result = searchDoc(e.target.value);
 
-  const onClose = useCallback(() => {
-    setIsOpen(false);
-  }, [setIsOpen]);
+    setSearchList(result.length > 0 ? result[0].result : []);
+  };
 
-  useDocSearchKeyboardEvents({
-    isOpen,
-    onOpen,
-    onClose,
-    searchButtonRef,
-  });
+  const Line = ({ children, href }: { children: React.ReactNode; href: string }) => {
+    return (
+      <Link
+        href={href}
+        className="relative flex cursor-pointer select-none items-center gap-2 rounded-sm px-4 py-3 text-sm text-inherit outline-none hover:bg-accent hover:text-accent-foreground [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+      >
+        {children}
+      </Link>
+    );
+  };
 
   return (
-    <>
-      <Button
-        onClick={() => setIsOpen(true)}
-        variant="outline"
-        className="relative h-8 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-32 lg:w-48 xl:w-56"
-      >
-        <span className="inline-flex">Try Search</span>
-        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-          <span className="text-xs">⌘</span>K
-        </kbd>
-      </Button>
-      {isOpen &&
-        createPortal(
-          <DocSearchModal
-            initialScrollY={window.scrollY}
-            appId={siteConfig.docSearch.appId}
-            apiKey={siteConfig.docSearch.apiKey}
-            indexName={siteConfig.docSearch.indexName}
-            onClose={onClose}
-            placeholder="搜索文章内容"
-            hitComponent={({ hit, children }) => <Link href={hit.url}>{children}</Link>}
-          />,
-          document.body,
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="hidden md:block">
+          <Button
+            variant="outline"
+            className="relative h-8 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-32 lg:w-48 xl:w-56"
+          >
+            <span className="inline-flex">Try Search</span>
+            <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+              <span className="text-xs">⌘</span>K
+            </kbd>
+          </Button>
+        </div>
+      </DialogTrigger>
+      <DialogContent hideCloseIcon className="gap-0 p-0">
+        {/* Header 仅仅防止报错 */}
+        <DialogHeader>
+          <DialogTitle className="m-0 p-0"></DialogTitle>
+          <DialogDescription className="m-0 p-0"></DialogDescription>
+        </DialogHeader>
+
+        <div className="flex items-center border-b px-3">
+          <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+          <Input
+            className="border-none focus-visible:ring-0"
+            placeholder="搜索文章"
+            onChange={debounce(onSearch, 200)}
+          />
+        </div>
+        {searchList.length == 0 && (
+          <div className="h-[300px]">
+            <div className="flex h-full w-full flex-col items-center justify-center">
+              <Search className="h-16 w-16 shrink-0 opacity-50" />
+              <div className="text-foreground/60">输入关键词搜索</div>
+            </div>
+          </div>
         )}
-    </>
+        {searchList.length > 0 && (
+          <div className="max-h-[300px] overflow-y-auto overflow-x-hidden px-2 py-1 text-foreground md:max-h-[calc(100vh-300px)]">
+            {searchList.map((item) => {
+              if (item.doc.type == 'heading') {
+                return (
+                  <Line key={item.doc.id} href={item.doc.link}>
+                    <Heading /> {item.doc.headings}
+                  </Line>
+                );
+              }
+              return (
+                <Line key={item.doc.id} href={item.doc.link}>
+                  <File />
+                  <div>
+                    <div className="line-clamp-2 w-full">{item.doc.content}</div>
+                    <div className="overflow-hidden text-xs font-bold text-muted-foreground">
+                      {item.doc.headings}
+                    </div>
+                  </div>
+                </Line>
+              );
+            })}
+          </div>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 }
