c97dcdfe770b17f8191101a93f334aef064c0610
json-q <112090173+json-q@users.noreply.github.com>
2025-02-23 23:56:39 +0800

    fix: build lint type error

--- app/auth.ts
+++ app/auth.ts
@@ -1,8 +1,8 @@
-import NextAuth, { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub as unknown as AuthOptions['providers'][number]],
+  providers: [GitHub as any],
   // session:{
   //   maxAge: 60 * 60 * 24,
   // },
