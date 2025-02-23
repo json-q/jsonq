f4fd6c94b061a1002b7c3ede33df995c25cb2703
JSQ <j996730508@163.com>
2025-02-24 00:24:41 +0800

    fix: build lint type error

--- app/auth.ts
+++ app/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub as any],
+  providers: [GitHub as never],
   // session:{
   //   maxAge: 60 * 60 * 24,
   // },
