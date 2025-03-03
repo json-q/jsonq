df058043550e637162c6327f2f0bac1a7fc23fd8
json-q <112090173+json-q@users.noreply.github.com>
2025-03-03 15:28:44 +0800

    chore: update tailwindcss to v4 and so on (#10)
    * chore(deps): update dependency tailwindcss to v4, next to 15.2.0
    
    * chore: upgrade deps

---
+++ app/tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+            a: {
+              color: 'var(--color-blue-600)',
+              '&:hover': {
+                color: 'var(--color-blue-800)',
+              },
+              code: { color: 'var(--color-blue-600)' },
+            },
+            code: {
+              color: 'var(--color-pink-600)',
+            },
+            hr: {
+              marginTop: '1rem',
+              marginBottom: '1rem',
+            },
+            img: {
+              marginTop: 0,
+              marginBottom: 0,
+            },
+            blockquote: {
+              p: {
+                color: 'var(--color-slate-600)',
+              },
+            },
+            figure: {
+              marginTop: '0.5rem',
+              marginBottom: '0.5rem',
+            },
+            ul: {
+              'padding-inline-start': '0.5rem',
+            },
+          },
+        },
+      },
+    },
+  },
+};
