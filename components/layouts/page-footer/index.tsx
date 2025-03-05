62f9eca76eb732ee3422cabda4a760acd44d7403
JSQ <j996730508@163.com>
2025-03-05 13:20:57 +0800

    chore: add cva vscode setting and page style

--- components/layouts/page-footer/index.tsx
+++ components/layouts/page-footer/index.tsx
@@ -6,7 +6,7 @@ export default function PageFooter() {
     <footer className="mt-auto box-border border-t bg-zinc-50 p-2 md:p-4 dark:bg-zinc-900">
       <div className="flex flex-col items-center space-y-4 text-center">
         <div className="flex flex-col items-center gap-4 text-sm text-zinc-600 md:flex-row md:text-base dark:text-zinc-300">
-          <span aria-label="copyright">©2025 风希落</span>
+          <span aria-label="copyright">©2025 Jsonq</span>
           <Link
             aria-label="beian"
             href="https://beian.miit.gov.cn"
