c7311bf1b8e20de2cf70e9de398ae9d230b42c0b
JSQ <j996730508@163.com>
2025-03-13 20:57:44 +0800

    fix: static page url redirect

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,12 @@ const nextConfig: NextConfig = {
   // https://github.com/hashicorp/next-mdx-remote?tab=readme-ov-file#installation
   transpilePackages: ['next-mdx-remote'],
   images: {
+    // 静态导出无法优化
     unoptimized: true,
   },
+
+  // 可选：阻止自动 `/me` -> `/me/`
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
