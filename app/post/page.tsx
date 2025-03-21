8089603ad2d748b4a852abde8c8c68fc1b288750
JSQ <j996730508@163.com>
2025-03-21 15:03:22 +0800

    feat: improve accessibility

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -11,21 +11,23 @@ export default async function PostList() {
   const postList = await getPostList();
 
   return (
-    <ul className="m-0 p-0">
+    <ul className="m-0 p-0" role="list">
       {postList.map((post) => (
-        <Link
+        <li
           key={post.url}
-          href={post.url}
-          className="!text-inherit no-underline hover:text-inherit dark:hover:text-inherit"
+          className="dark:hover:bg-accent mb-4 flex w-full flex-col rounded border hover:bg-slate-50 dark:border-slate-600"
         >
-          <li className="dark:hover:bg-accent mb-4 flex w-full flex-col rounded border p-4 hover:bg-slate-50 dark:border-slate-600">
+          <Link
+            href={post.url}
+            className="p-4 !text-inherit no-underline hover:text-inherit dark:hover:text-inherit"
+          >
             <h5 className="font-bold">{post.title}</h5>
-            <div className="mt-2 flex justify-between text-sm text-slate-400">
+            <div className="mt-2 flex justify-between text-sm text-slate-600 dark:text-slate-400">
               <time>{dayjs(post.publishedAt).format('YYYY-MM-DD')}</time>
               <time>{post.readingTime}</time>
             </div>
-          </li>
-        </Link>
+          </Link>
+        </li>
       ))}
     </ul>
   );
